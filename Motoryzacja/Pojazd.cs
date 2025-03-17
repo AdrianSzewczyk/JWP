0fdc9fe8d2455605673a4f751e106b8e5a1ee927
AdrianSzewczyk <148904696+AdrianSzewczyk@users.noreply.github.com>
2025-03-17 14:09:52 +0100

    Add files via upload

---
+++ Motoryzacja/Pojazd.cs
@@ -0,0 +1,65 @@
+﻿namespace Motoryzacja
+{
+    public class Pojazd
+    {
+        public string Nazwa { get; set; } = "";
+        private int liczbaKół;
+        public int IleKół
+        {
+            get { return liczbaKół; }
+            set
+            {
+                if (value < 0) value = 0;
+                this.liczbaKół = value;
+            }
+        }
+
+        private double prędkość;
+        public double Prędkość
+        {
+            get { return prędkość; }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                this.prędkość = value;
+            }
+        }
+
+        
+        private static short liczbaPojazdów;
+
+        
+        public short Lp { get; private set; }
+
+        
+        static Pojazd()
+        {
+            liczbaPojazdów = 0; 
+        }
+
+        
+        public Pojazd()
+        {
+            liczbaPojazdów++; 
+            Lp = liczbaPojazdów; 
+        }
+
+        
+        public Pojazd(string n, int lk, double pr) : this() 
+        {
+            Nazwa = n;
+            Prędkość = pr;
+            IleKół = lk;
+        }
+
+        public Pojazd(string n, double pr) : this(n, 4, pr) 
+        {
+        }
+
+        
+        public override string ToString()
+        {
+            return $"Lp: {Lp}/{liczbaPojazdów}, Nazwa: {Nazwa}, Liczba Kół: {IleKół}, Prędkość: {Prędkość} km/h";
+        }
+    }
+}
