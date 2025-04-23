fb25ae8bc4bc6f55faaf868652320267e9e5680b
AdrianSzewczyk <148904696+AdrianSzewczyk@users.noreply.github.com>
2025-04-23 12:50:11 +0200

    Add files via upload

---
+++ Lab7/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Klasy
+{
+    [Serializable]
+    public class Student
+    {
+        public string Nazwisko { get; set; } = "";
+        public int Ocena { get; set; }
+        public override string ToString()
+        {
+            return $"{Nazwisko} {Ocena}";
+        }
+    }
+    public class Grupa
+    {
+        public string Nazwa { get; set; } = "";
+        public int LiczbaStudentów{get;}
+        public List<Student> Studenci { get; set; }
+        public int ŚredniaOcen { get; }
+        public void Wyświetl(ListBox l)
+        {
+            l.Items.Add($"Grupa:{Nazwa}");
+            foreach (var s in Studenci)
+            {
+                l.Items.Add(s);
+            }
+            l.Items.Add($"Liczba Studentów:{LiczbaStudentów}");
+            l.Items.Add($"Średnia Ocen:{ŚredniaOcen}");
+        }
+
+    }
+}
---
+++ Lab7/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Klasy;
+
+namespace ZadA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {   
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMelduj_Click(object sender, RoutedEventArgs e)
+        {
+            string data = DateTime.Now.ToShortDateString();
+            FileStream fs = new FileStream(@"C:\Users\Student\Desktop\LaboratoriumSzewczyk\Laboratorium 7\ZadA\rejestr.txt", FileMode.Append, FileAccess.Write,FileShare.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(data);
+            sw.Close();
+        }
+
+        private void btnCzytaj_Click(object sender, RoutedEventArgs e)
+        {
+            lsLista.Items.Clear();
+
+            List<int> listaDanych = new List<int>();
+            using (FileStream fsDane = new FileStream(
+                @"C:\Users\Student\Desktop\LaboratoriumSzewczyk\Laboratorium 7\ZadA\dane.txt",
+                FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader srDane = new StreamReader(fsDane))
+            {
+                string linia;
+                while ((linia = srDane.ReadLine()) != null)
+                {
+                    lsLista.Items.Add($"Liczba: {linia}");
+
+                    
+                    if (int.TryParse(linia, out int liczba))
+                    {
+                        listaDanych.Add(liczba);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Nieprawidłowa liczba: {linia}");
+                    }
+                }
+            }
+            var srednia = listaDanych.Average();
+            var najmniejsza = listaDanych.Min();
+            var najwieksza = listaDanych.Max();
+            lblŚrednia.Content = srednia.ToString();
+            lblNajwieksza.Content = najwieksza.ToString();
+            lblNajmniejsza.Content = najmniejsza.ToString();
+            
+        }
+
+
+        private void btnDodaj_Click(object sender, RoutedEventArgs e)
+        {
+
+            FileStream fsDane = new FileStream(@"C:\Users\Student\Desktop\LaboratoriumSzewczyk\Laboratorium 7\ZadA\dane.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            StreamWriter swDane = new StreamWriter(fsDane);
+            //sw.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                swDane.WriteLine(i);
+            }
+            swDane.Close();
+        }
+    }
+}
